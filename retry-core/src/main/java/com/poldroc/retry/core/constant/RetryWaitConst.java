84aa10f41a97a20d81673e44f9c33f1c0cd3b214
Poldroc <engroc@foxmail.com>
2024-07-12 12:07:01 +0800

    feat: 添加重试等待时间常量

---
+++ retry-core/src/main/java/com/poldroc/retry/core/constant/RetryWaitConst.java
@@ -0,0 +1,44 @@
+package com.poldroc.retry.core.constant;
+
+/**
+ * 重试等待时间常量
+ *
+ * @author Poldroc
+ * @date 2024/7/11
+ */
+
+public final class RetryWaitConst {
+    private RetryWaitConst() {
+    }
+
+    /**
+     * 默认基础值
+     * 1s
+     */
+    public static final long DEFAULT_VALUE_MILLS = 1000L;
+
+    /**
+     * 最小等待时间
+     */
+    public static final long DEFAULT_MIN_MILLS = 0L;
+
+    /**
+     * 最大等待时间
+     * 30min
+     */
+    public static final long DEFAULT_MAX_MILLS = 30 * 60 * 1000L;
+
+    /**
+     * 增加的毫秒数因数
+     * 默认为 2S
+     */
+    public static final double INCREASE_MILLS_FACTOR = 2000;
+
+    /**
+     * 因数
+     * 默认为黄金分割比
+     */
+    public static final double MULTIPLY_FACTOR = 1.618;
+
+
+}
