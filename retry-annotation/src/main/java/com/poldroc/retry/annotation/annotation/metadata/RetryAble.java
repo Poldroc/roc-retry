d1ff37752cd8c12bf9a95dc1fb9f6ac681ed08ae
Poldroc <engroc@foxmail.com>
2024-07-18 21:51:08 +0800

    feat: 可重试注解、重试等待注解

---
+++ retry-annotation/src/main/java/com/poldroc/retry/annotation/annotation/metadata/RetryAble.java
@@ -0,0 +1,26 @@
+package com.poldroc.retry.annotation.annotation.metadata;
+
+import com.poldroc.retry.annotation.handler.RetryAbleHandler;
+
+import java.lang.annotation.*;
+/**
+ * 可重试注解
+ * 1.用于注解之上用于指定重试信息
+ * @see com.poldroc.retry.api.context.RetryContext
+ * @author Poldroc
+ * @date 2024/7/13
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE)
+@Inherited
+public @interface RetryAble {
+
+    /**
+     * 对应的注解处理器
+     * @return class 信息
+     */
+    Class<? extends RetryAbleHandler> value();
+
+}
---
+++ retry-annotation/src/main/java/com/poldroc/retry/annotation/annotation/metadata/RetryWaitAble.java
@@ -0,0 +1,27 @@
+package com.poldroc.retry.annotation.annotation.metadata;
+
+import com.poldroc.retry.annotation.handler.RetryWaitAbleHandler;
+
+import java.lang.annotation.*;
+
+/**
+ * 可重试等待的注解
+ * 1. 用于注解之上指定等待注解的处理信息
+ * @see com.poldroc.retry.api.context.RetryWaitContext
+ * @author Poldroc
+ * @date 2024/7/15
+ */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE)
+@Inherited
+public @interface RetryWaitAble {
+
+    /**
+     * 对应的注解处理器
+     * @return class 信息
+     */
+    Class<? extends RetryWaitAbleHandler> value();
+
+}
